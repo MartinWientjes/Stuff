d77ca448081dbb0b772b321aeba65aa80786c909
Martin Wientjes <MartinWientjes@users.noreply.github.com>
2016-12-04 15:31:12 +0100

    Delete mw3.jl

--- mw3.jl
+++
@@ -1,8 +0,0 @@
-function MW3()
-    mwData = Pkg.installed()
-    for l in sort(collect(keys(mwData)))
-        mwsubstring = "Pkg.add(\"" * lstrip(l) * "\")"
-        println(mwsubstring)
-    end
-end
-MW3()
